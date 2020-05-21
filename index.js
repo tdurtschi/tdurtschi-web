80a091d1511fa4e904fe3ddf1b5105533358058e
Teagan Durtschi <tdurtschi@pivotal.io>
2020-05-21 18:20:32 -0400

    Add current bug website

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-var port = 8080;
-var express = require("express");
+var port = 9000
+var express = require("express")
 
-var app = express();
-app.use('/', express.static('c:\\users\\tdurf\\workspace\\personalSite\\bin'));
-app.listen(port);
+var app = express()
+app.use("/", express.static("./bin"))
+app.listen(port)
